c502b80fd7f1f67fd60f150465cbb546b199f6c4
AndyGrigs <97991761+AndyGrigs@users.noreply.github.com>
2024-10-11 11:39:25 +0200

    +

---
+++ config/storybook/decorators/styleDecorator.ts
@@ -0,0 +1,4 @@
+import 'app/styles/index.scss'
+import { StoryFn } from '@storybook/react'
+
+export const StyleDecorator = (story: ()=> StoryFn) => story()
--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from "@storybook/react";
+import { StyleDecorator} from './decorators/styleDecorator'; // Adjust the path as necessary
 
 const preview: Preview = {
   parameters: {
@@ -9,6 +10,9 @@ const preview: Preview = {
       },
     },
   },
+ 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  decorators: [StyleDecorator],
 };
 
 export default preview;
